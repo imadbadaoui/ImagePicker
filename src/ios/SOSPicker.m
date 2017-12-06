8c0e8cccc10edd475313f88d8b976c10cbe88063
Imad BADAOUI <i.badaoui@MacBook-Pro-de-SQLI.local>
2017-12-06 17:44:52 +0100

    Change popup fix

--- src/ios/SOSPicker.m
+++ src/ios/SOSPicker.m
@@ -35,7 +35,7 @@ typedef enum : NSUInteger {
     // [PHPhotoLibrary requestAuthorization:]
     // this method works only when it is a first time, see
     // https://developer.apple.com/library/ios/documentation/Photos/Reference/PHPhotoLibrary_Class/
-
+    
     PHAuthorizationStatus status = [PHPhotoLibrary authorizationStatus];
     if (status == PHAuthorizationStatusAuthorized) {
         NSLog(@"Access has been granted.");
@@ -47,15 +47,15 @@ typedef enum : NSUInteger {
     } else if (status == PHAuthorizationStatusRestricted) {
         NSLog(@"Access has been restricted. Change your setting > Privacy > Photo enable");
     }
-
+    
     CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 - (void) getPictures:(CDVInvokedUrlCommand *)command {
-
+    
     NSDictionary *options = [command.arguments objectAtIndex: 0];
-
+    
     self.outputType = [[options objectForKey:@"outputType"] integerValue];
     BOOL allow_video = [[options objectForKey:@"allow_video" ] boolValue ];
     NSInteger maximumImagesCount = [[options objectForKey:@"maximumImagesCount"] integerValue];
@@ -63,12 +63,12 @@ typedef enum : NSUInteger {
     NSString * message = [options objectForKey:@"message"];
     BOOL disable_popover = [[options objectForKey:@"disable_popover" ] boolValue];
     if (message == (id)[NSNull null]) {
-      message = nil;
+        message = nil;
     }
     self.width = [[options objectForKey:@"width"] integerValue];
     self.height = [[options objectForKey:@"height"] integerValue];
     self.quality = [[options objectForKey:@"quality"] integerValue];
-
+    
     self.callbackId = command.callbackId;
     [self launchGMImagePicker:allow_video title:title message:message disable_popover:disable_popover maximumImagesCount:maximumImagesCount];
 }
@@ -83,15 +83,16 @@ typedef enum : NSUInteger {
     picker.colsInPortrait = 4;
     picker.colsInLandscape = 6;
     picker.minimumInteritemSpacing = 2.0;
-
+    
     if(!disable_popover) {
         picker.modalPresentationStyle = UIModalPresentationPopover;
-
+        
         UIPopoverPresentationController *popPC = picker.popoverPresentationController;
-        popPC.permittedArrowDirections = UIPopoverArrowDirectionDown;
+        popPC.permittedArrowDirections = UIPopoverArrowDirectionAny;
         popPC.sourceView = picker.view;
+        //popPC.sourceRect = nil;
     }
-
+    
     [self.viewController showViewController:picker sender:nil];
 }
 
@@ -107,11 +108,11 @@ typedef enum : NSUInteger {
     CGFloat targetHeight = frameSize.height;
     CGFloat scaleFactor = 0.0;
     CGSize scaledSize = frameSize;
-
+    
     if (CGSizeEqualToSize(imageSize, frameSize) == NO) {
         CGFloat widthFactor = targetWidth / width;
         CGFloat heightFactor = targetHeight / height;
-
+        
         // opposite comparison to imageByScalingAndCroppingForSize in order to contain the image within the given bounds
         if (widthFactor == 0.0) {
             scaleFactor = heightFactor;
@@ -124,16 +125,16 @@ typedef enum : NSUInteger {
         }
         scaledSize = CGSizeMake(floor(width * scaleFactor), floor(height * scaleFactor));
     }
-
+    
     UIGraphicsBeginImageContext(scaledSize); // this will resize
-
+    
     [sourceImage drawInRect:CGRectMake(0, 0, scaledSize.width, scaledSize.height)];
-
+    
     newImage = UIGraphicsGetImageFromCurrentImageContext();
     if (newImage == nil) {
         NSLog(@"could not scale image");
     }
-
+    
     // pop the context to get back to the default
     UIGraphicsEndImageContext();
     return newImage;
@@ -160,29 +161,29 @@ typedef enum : NSUInteger {
 - (void)assetsPickerController:(GMImagePickerController *)picker didFinishPickingAssets:(NSArray *)fetchArray
 {
     [picker.presentingViewController dismissViewControllerAnimated:YES completion:nil];
-
+    
     NSLog(@"GMImagePicker: User finished picking assets. Number of selected items is: %lu", (unsigned long)fetchArray.count);
-
+    
     NSMutableArray * result_all = [[NSMutableArray alloc] init];
     CGSize targetSize = CGSizeMake(self.width, self.height);
     NSFileManager* fileMgr = [[NSFileManager alloc] init];
     NSString* docsPath = [NSTemporaryDirectory()stringByStandardizingPath];
-
+    
     NSError* err = nil;
     int i = 1;
     NSString* filePath;
     CDVPluginResult* result = nil;
-
+    
     for (GMFetchItem *item in fetchArray) {
-
+        
         if ( !item.image_fullsize ) {
             continue;
         }
-
+        
         do {
             filePath = [NSString stringWithFormat:@"%@/%@%03d.%@", docsPath, CDV_PHOTO_PREFIX, i++, @"jpg"];
         } while ([fileMgr fileExistsAtPath:filePath]);
-
+        
         NSData* data = nil;
         if (self.width == 0 && self.height == 0) {
             // no scaling required
@@ -210,7 +211,7 @@ typedef enum : NSUInteger {
             UIImage* image = [UIImage imageNamed:item.image_fullsize];
             UIImage* scaledImage = [self imageByScalingNotCroppingForSize:image toSize:targetSize];
             data = UIImageJPEGRepresentation(scaledImage, self.quality/100.0f);
-
+            
             if (![data writeToFile:filePath options:NSAtomicWrite error:&err]) {
                 result = [CDVPluginResult resultWithStatus:CDVCommandStatus_IO_EXCEPTION messageAsString:[err localizedDescription]];
                 break;
@@ -223,14 +224,14 @@ typedef enum : NSUInteger {
             }
         }
     }
-
+    
     if (result == nil) {
         result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsArray:result_all];
     }
-
+    
     [self.viewController dismissViewControllerAnimated:YES completion:nil];
     [self.commandDelegate sendPluginResult:result callbackId:self.callbackId];
-
+    
 }
 
 //Optional implementation:
